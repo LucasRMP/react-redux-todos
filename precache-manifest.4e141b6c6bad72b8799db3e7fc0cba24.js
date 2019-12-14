7218f3c0204c4fd50178d8996b7c48fe9b6b9001
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2019-12-14 18:49:53 -0200

    Updates

---
+++ precache-manifest.4e141b6c6bad72b8799db3e7fc0cba24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f34c2c6d4955a5989cabfa84eceef6",
+    "url": "/react-redux-todos/index.html"
+  },
+  {
+    "revision": "d5ae426220433cd4377b",
+    "url": "/react-redux-todos/static/js/2.5cff1724.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-todos/static/js/2.5cff1724.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4aac3f2bebd3eb1da435",
+    "url": "/react-redux-todos/static/js/main.61a1796a.chunk.js"
+  },
+  {
+    "revision": "d0d32b6056736b0eba8b",
+    "url": "/react-redux-todos/static/js/runtime-main.3c65e098.js"
+  }
+]);
